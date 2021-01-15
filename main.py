689bc8872eb19eb85e4740c282a9f91b839430c3
Kavya Somala <somala.kavya123@gmail.com>
2021-01-15 09:52:13 -0500

    Updated and look of the code and switched over to VSCode instead of Repl.It since this VSCode seems to work better and run faster.

--- main.py
+++ main.py
@@ -28,11 +28,9 @@ elif answer.lower() in negative_answers:
 else:
   print("\nummm....that's an answer I do not understand. I am still a robot you know. I'll assume you meant yes :)")
   
-  print("This textbased game will take you on an adventure through a series of yes or no answers.  but could be fun anyway if you're ready to give it a try!\n")
+print("\n In this text based roleplaying game, you (the player) will have a chance to navigate the world in Dr. Sunshine's Sublime Circus through a series of yes or no answers. \nYou will get the chance to be whisked away to a festive circus nestled in a whimsical, verdant valley. \nAfter discovering a great and dangerous secret about the circus, you must try and escape the piercing gaze of Dr. Sunshine's true, terrifying form. \nWill you be able to get out, or will you become a more permanent fixture of the circus?\n")
 
-print("\n In this text based roleplaying game, you (the player) will have a chance to navigate the world in Dr. Sunshine's Sublime Circus through a series of yes or no answers. You will get the chance to be whisked away to a festive circus nestled in a whimsical, verdant valley. After discovering a great and dangerous secret about the circus, you must try and escape the piercing gaze of Dr. Sunshine's true, terrifying form. Will you be able to get out, or will you become a more permanent fixture of the circus?\n")
-
-time.sleep(10)
+time.sleep(5)
 
 answer = input("Now that you know what's going on, let's begin! I will be your guide, your DM I suppose.  Ready to start? ")
 
@@ -45,7 +43,7 @@ elif answer in negative_answers:
 else:
   print("\nummm....that's an answer I do not understand. I am still a robot you know. I'll assume you meant yes :)")
 
-print("\nYou and your group find yourself in a tavern. Though it wouldn't have been renowned for it's interior decorating, it holds an undeniable allure that drew your party to it. You notice the bright colorful posters plastered across the walls of the tavern. All of them identical, featuring a a sharp featured, half-masked man. From the distance, you can see that the posters say, 'Come one, come all! Got a case of the blues? Visit Dr.Sunshine's Sublime Circus of Wonder and Awe. He'll fix you right up!'")
+print("\nYou and your group find yourself in a tavern. Though it wouldn't have been renowned for it's interior decorating, it holds an undeniable allure that drew your party to it. \nYou notice the bright colorful posters plastered across the walls of the tavern. All of them identical, featuring a a sharp featured, half-masked man. \nFrom the distance, you can see that the posters say, 'Come one, come all! Got a case of the blues? Visit Dr.Sunshine's Sublime Circus of Wonder and Awe. He'll fix you right up!'")
 
 answer = input("\nWould you like to approach the posters? ")
 
@@ -56,6 +54,6 @@ elif answer in negative_answers:
   print("\nJust as your party came to the decision that the poster was quite boring and unworthy of your time, a patron of the tavern calls out to you: \n 'oi! You lot 'ave got to visit that there circus! I ain't never 'ad more fun in me life, and I've led quite a life! All you got t'do is touch it.'")
   print("\nRelectunt but intrigued by the patron's recommendation, your party aproaches the posters.")  
 
-print("\nAs one of the players reaches out to touch the garish poster, poster, a flutter of confetti spews from the smiling mouth of the central figure, swirling around you all like a small tornado. An out-of-tune trumpet blares a fanfare, drowning out the noise of surprised tavern goers.\n Suddenly, the confetti fades, and you are standing in a tavern no longer. Rather, the twilight of a distant sun illuminates a verdant valley. The sounds of revelry and festivity hang in the air, their source dominating your view: a collection of brightly colored circus tents. Farther along the path, at the end of the valley, a huge big top looms above all else, dominating your view.")
+print("\nAs one of the players reaches out to touch the garish poster, a flutter of confetti spews from the smiling mouth of the central figure, swirling around you all like a small tornado. An out-of-tune trumpet blares a fanfare, drowning out the noise of surprised tavern goers.\n Suddenly, the confetti fades, and you are standing in a tavern no longer. Rather, the twilight of a distant sun illuminates a verdant valley. The sounds of revelry and festivity hang in the air, their source dominating your view: a collection of brightly colored circus tents. Farther along the path, at the end of the valley, a huge big top looms above all else, dominating your view.")
 
-answer = print("What would you like to do?")
+answer = print("\nWhat would you like to do?")

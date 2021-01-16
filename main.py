7ff92cffacc2c438cf1e80e7da96ac60c649fc90
Kavya Somala <somala.kavya123@gmail.com>
2021-01-15 19:50:22 -0500

    Got the story line started, worked out a few more bugs, and added more affirmative answers. Need to write out the rest of the storyline and make them into choices. Also made a longer list of bugs to fix and features that need to be integrated at some point.

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ https://drive.google.com/file/d/1exUoUk4gNaJa6SB4Fz7STngM8Cu_njdN/view
 Thank you for being here :)
 '''
 
+#BUGS TO FIX:
 #make a function that adresses what to do if they say something unexpected
 #make a function to adress if the user ever wants to exit the game
+#remove choices and make it into nlp instead
+#start bot when user comes online instead of waiting for them to type start
+#help command doesn't always work
 
 import time
 import os
@@ -16,19 +20,15 @@ import discord
 from discord.ext import commands
 
 #variables
-affirmative_answers = ("yes", "yeah", "sure", "yup")
-negative_answers = ("no", "nope", "nah", "naw", "not a chance", "sorry")
+affirmative_answers = ("yes", "yeah", "sure", "yup", "okay", "of course", "alright", "certainly", "absolutely", "by all means", "yup", "ok", "okie", "yea")
+negative_answers = ("no", "nope", "nah", "naw", "not a chance", "sorry", "never", "no way")
 exit_commands = ("quit", "pause", "exit", "goodbye", "bye", "later", "stop")
 all_responses = affirmative_answers + negative_answers + exit_commands
 no_count = 0
 
-TOKEN = '****************'
+TOKEN = "***************************************"
 
 client = commands.Bot(command_prefix = '.')
-
-@client.event
-async def on_command_error(ctx, error):
-    await ctx.send(f'Error. Try .help for a list of possible responses. ({error})')
 
 client.remove_command('help')
 
@@ -37,6 +37,8 @@ async def help(ctx):
     embed = discord.Embed(
         colour = discord.Colour.green())
     embed.set_author(name='Help : list of acceptable answers or topics')
+    embed.add_field(name = ".help", value='Brings up menu of possible responses and topics available.', inline=False)
+    embed.add_field(name = ".start", value='Begins the adventure.', inline=False)
     for i in all_responses:
       embed.add_field(name='.' + i, value='Self Explained', inline=False)
     await ctx.send(embed=embed)
@@ -46,73 +48,152 @@ async def start(ctx):
   await ctx.send("Welcome to Dr. Sunshine's Sublime Circus.")
   await ctx.send("Would you like this adventure's overview?")
   def check(msg):
-          return msg.author == ctx.author and msg.channel == ctx.channel
-
+    return msg.author == ctx.author and msg.channel == ctx.channel
+  
   msg = await client.wait_for("message", check=check)
   if msg.content.lower() in affirmative_answers:
     await ctx.send("Yay!")
-    print("JUST KIDDING I'M HERE")
-  elif msg.content.lower() in negative_answers:
+  elif msg.content.lower() in negative_answers or msg.content.lower() in exit_commands:
     await ctx.send("Are you sure?")
     msg = await client.wait_for("message", check=check)
-    if msg.content.lower() in affirmative_answers:
+    if msg.content.lower() in affirmative_answers or msg.content.lower() in exit_commands:
       await ctx.send("Okie, bye!")
-      quit()
+      exit()
     elif msg.content.lower() in negative_answers:
       await ctx.send("Knew you couldn't resist haha")
+    else:
+      await ctx.send("Error. I do not understand that command. Try .help for a current list of possible responses.")
+      exit()
   else:
-    await ctx.send("I don't understand.")
-
+    await ctx.send("Error. I do not understand that command. Try .help for a current list of possible responses.")
+    exit()
 
   await ctx.send("In this text based roleplaying game, you (the player) will have a chance to navigate the world in Dr. Sunshine's Sublime Circus through a series of given answers.")
   await ctx.send("You will get the chance to be whisked away to a festive circus nestled in a whimsical, verdant valley.")
   await ctx.send("After discovering a great and dangerous secret about the circus, you must try and escape the piercing gaze of Dr. Sunshine's true, terrifying form.")
   await ctx.send("Will you be able to get out, or will you become a more permanent fixture of the circus?")
+  await ctx.send("Now that you know what's going on, let's begin!")
+  await ctx.send("If you would like to exit at any point, just say any exit command.")
+  await ctx.send("Ready to start?")
 
-'''
-
-  print("\nIn this text based roleplaying game, you (the player) will have a chance to navigate the world in Dr. Sunshine's Sublime Circus through a series of given answers. \nYou will get the chance to be whisked away to a festive circus nestled in a whimsical, verdant valley. \nAfter discovering a great and dangerous secret about the circus, you must try and escape the piercing gaze of Dr. Sunshine's true, terrifying form. \nWill you be able to get out, or will you become a more permanent fixture of the circus?\n")
-
-  time.sleep(5)
-
-  answer = input("Now that you know what's going on, let's begin! I will be your guide, your DM I suppose.  Ready to start? ")
-  chat_check(answer)
-
-  if answer:
-      print("\nPerfect, let's go!")
-  elif not answer:
-    if no_count > 0:
-      print("\nNow you're just being stubborn! Let's go anyway!")
-    no_count += 1
-
-  print("\nYou and your group find yourself in a tavern. Though it wouldn't have been renowned for it's interior decorating, it holds an undeniable allure that drew your party to it. \nYou notice the bright colorful posters plastered across the walls of the tavern. All of them identical, featuring a a sharp featured, half-masked man. \nFrom the distance, you can see that the posters say, 'Come one, come all! Got a case of the blues? Visit Dr.Sunshine's Sublime Circus of Wonder and Awe. He'll fix you right up!'")
-  answer = input("\nWhat would you like to do? \n a - approach the posters \n b - order a drink \n c - look around the tavern")
-
-  chat_check(answer)
-
-  if answer:
-    print("\nYour party approaches the posters lining the tavern, intrigued.")
-  elif not answer:
-    no_count += 1
-    print("\nJust as your party came to the decision that the poster was quite boring and unworthy of your time, a patron of the tavern calls out to you: \n 'oi! You lot 'ave got to visit that there circus! I ain't never 'ad more fun in me life, and I've led quite a life! All you got t'do is touch it.'")
-    print("\nRelectunt but intrigued by the patron's recommendation, your party aproaches the posters.")  
-
-  print("\nAs one of the players reaches out to touch the garish poster, a flutter of confetti spews from the smiling mouth of the central figure, swirling around you all like a small tornado. An out-of-tune trumpet blares a fanfare, drowning out the noise of surprised tavern goers.\n Suddenly, the confetti fades, and you are standing in a tavern no longer. Rather, the twilight of a distant sun illuminates a verdant valley. The sounds of revelry and festivity hang in the air, their source dominating your view: a collection of brightly colored circus tents. Farther along the path, at the end of the valley, a huge big top looms above all else, dominating your view.")
-  print("\nWhat would you like to do?")
-
-  end_game()
-  quit()
+  msg = await client.wait_for("message", check=check)
+  if msg.content.lower() in affirmative_answers:
+    await ctx.send("Perfect, let's go!")
+    await ctx.send("Here is the map for this adventure:")
+    await ctx.send("https://preview.redd.it/gx79c9oqey561.png?width=1700&format=png&auto=webp&s=c2a5b629dfd346bad797a764946ed91bb4323952")
+  elif msg.content.lower() in negative_answers or msg.content.lower() in exit_commands:
+    await ctx.send("Aww. Sorry to see you go I guess. Bye! Come back soon!")
+    exit()
+  else:
+    await ctx.send("Error. I do not understand that command. Try .help for a current list of possible responses.")
+    exit()
   
-def unknown_response():
-  print("\nSorry, I didn't understand what you said? Could you please try again?")
-  chat()
+  await ctx.send("You and your group find yourself in a tavern.")
+  await ctx.send("Though it wouldn't have been renowned for it's interior decorating, it holds an undeniable allure that drew your party to it.")
+  await ctx.send("You notice the bright colorful posters plastered across the walls of the tavern. All of them identical, featuring a a sharp featured, half-masked man.")
+  await ctx.send("From the distance, you can see that the posters say, 'Come one, come all! Got a case of the blues? Visit Dr.Sunshine's Sublime Circus of Wonder and Awe. He'll fix you right up!'")
   
-def end_game():
-  print("You have reached the end of the game. Thank you for playing!")
- 
-def exit():
-  print("Okay, good bye! It was nice having you, please come back soon.")
-
-#def choices(answer): #eventually change these from multiple choice to NLP so the user has more freedom
-'''
+  await ctx.send("What would you like to do?")
+  await ctx.send("a - approach the posters \nb - find a table \nc - order a drink\nd - leave")
+  
+  msg = await client.wait_for("message", check=check)
+  print(msg.content.lower())  
+  if msg.content.lower() == 'a':
+    await ctx.send("Your party approaches the posters lining the tavern, intrigued.")
+  if msg.content.lower() == 'b':
+    await ctx.send("You find a table easily enough. The tavern is fairly empty, consisting only of a few patrons and the bartender wiping down the surfaces.")
+    await ctx.send("As you settle down, you hear a patron calling out to you: 'oi! You lot 'ave got to visit that there circus! I ain't never 'ad more fun in me life, and I've led quite a life! All you got t'do is touch it.'")
+    await ctx.send("Intrigued, a member of your party approaches the posters on the nearest wall. The rest of your party slowly follows.")
+  if msg.content.lower() == 'c':
+    await ctx.send("You approach the bartender, hoping to order a drink and perhaps some food. As you walk over, you hear a patron calling out to you: 'oi! You lot 'ave got to visit that there circus! I ain't never 'ad more fun in me life, and I've led quite a life! All you got t'do is touch it.")
+    await ctx.send("Now curious of the the posters because of the patron's recommendation, your party aproaches the posters.")  
+  if msg.content.lower() == 'd':
+    await ctx.send("Your party came to the conclusion that the allure you all felt to the tavern was for naught for the tavern was actually quite boring and unworthy of your time.")
+    await ctx.send("Just as you turn to leave, a patron calls out to you: 'oi! You lot 'ave got to visit that there circus! I ain't never 'ad more fun in me life, and I've led quite a life! All you got t'do is touch it.'")
+    await ctx.send("Relectunt but curious neverthless by the patron's recommendation, your party aproaches the posters.")  
+  
+  await ctx.send("Cautiously, a member of your party reaches out to touch the garish poster.")
+  await ctx.send("As soon as their hand comes into contact, a flutter of confetti spews from the smiling mouth of the central figure, swirling around you all like a small tornado.")
+  await ctx.send("An out-of-tune trumpet blares a fanfare, drowning out the noise of surprised tavern goers.")
+  await ctx.send("Suddenly, the confetti fades, and you are standing in a tavern no longer. Rather, the twilight of a distant sun illuminates a verdant valley.")
+  await ctx.send("The sounds of revelry and festivity hang in the air, their source dominating your view: a collection of brightly colored circus tents. Farther along the path, at the end of the valley, a huge big top looms above all else, dominating your view.")
+  await ctx.send("What would you like to do?")
+  
 client.run(TOKEN)
+
+'''
+Story to make into code:
+1. Gate of the Sun
+  Players are whisked away from the tavern to the midst of this
+  massive arch, located opposite the big top at the other end of
+  the valley. Players who inspect the stone arch notice crude
+  carvings of the sun covering its surface, but they see no
+  obvious means to return.
+  Once players get their bearings, they hear the following:
+    A voice barks from the nearby tent: "Well, are you just gonna
+    stand there, or are you ready for some fun!"
+  The voice comes from the visitor's center (area 2).
+2. Visitor's Center
+  Inside this tent stands a festive illusion (see Appendix A)
+  behind a wooden counter. The thin, smiling apparition of a
+  female elf seems real, even to the touch. Players would need
+  to succeed on a DC 30 Wisdom (Insight or Perception) check
+  in order to notice the minor inconsistencies present in the
+  illusion.
+  This illusion, like all other circus patrons, is controlled by
+  Dr. Sunshine, and acts like an over-excited, overly-interested
+  employee. It answers any questions players have about the
+  activities at the circus, giving exuberant, energetic replies.
+  No matter what else is said, the apparition emphasizes the
+  following:
+    "Oh, and you absolutely must see the doctor! His grand show
+    never fails to amaze, and I've never seen anyone leave it
+    without a smile on their face. Seriously! It's over at the big top,
+    you can't miss it."
+  Treasure. Players can grab a free souvenir from the gift
+  shop, either a hat with a spinning sunburst on top or a carved,
+  painted sunburst necklace.
+3. Mirror Maze
+A hunched barker stands outside this square building's single
+entrance, his voice rising as players approach:
+  "Step right up, try and navigate the maze of mirrors! Whoever
+  can manage it wins a wondrous prize!"
+The barker is in reality one of the lost* in disguise, its urges
+kept in check (for now) by Dr. Sunshine. The incredibly
+convincing illusion can only be seen through with a
+successful DC 30 Wisdom (Insight or Perception) check. The
+same goes for all other disguised sorrowsworn.
+Though the barker makes it seem like a challenge, the
+mirror maze is quite easy, and players can make it through
+with a successful DC 18 Intelligence (Investigation) or
+Wisdom (Insight) check. On a failure, the player leading the
+group walks into a mirror.
+Treasure. On a success, players navigate the maze and
+emerge on the other side, where the barker stands, ready
+with their prize:
+  Mirror of Farsight (requires attunement)
+    This gilded mirror worked in the shape of a sunburst glows
+    ever so slightly in sunlight.
+    As an action, the wielder can view any place they have seen
+    within 10 miles, as if using the scrying spell.
+    Curse. Once the true nature of the circus is revealed (see
+    Part 2), this mirror's curse manifests, unbeknownst to the
+    wielder. Afterward, there is a cumulative 20 percent chance
+    that when used, the mirror shows not the desired location,
+    but instead a small, dark room. In the corner stands an
+    unnaturally tall, skinny figure, facing away from the wielder --
+    an odraz (see Appendix A). If the wielder continues to gaze
+    upon the odraz, it emerges through the mirror and attacks.
+  
+    If players try to scry on Dr. Sunshine, either
+    through the mirror or their own magic, before
+    meeting him, they see him in his dressing room.
+    Turning to look directly at the viewer, Dr.
+    Sunshine reveals his maskless face, which
+    resembles stretched black leather. Through an
+    unnaturally large smile, Dr. Sunshine gives a wink
+    and a wave, whispering "shh, don't spoil the
+    surprise..." The scrying then fails, along with any
+    future attempt.
+  
+4. Ettin Beheadin'
